5352e5f494bd2f7c321ec4aa8f1fb2c1b811123f
Merveille Lubenzo <84930020+TekSniper@users.noreply.github.com>
2023-05-01 16:49:59 +0200

    Initialisation

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./node_modules/bulma/css/bulma.min.css">
+    <title>Document</title>
+    <style>
+        body{
+            min-height: 100vh;
+        }
+    </style>
+</head>
+
+<body class="has-background-info">
+    <div class="container">
+        <h2 class="title">
+            Bonjour !
+        </h2>
+    </div>
+
+</body>
+
+</html>
